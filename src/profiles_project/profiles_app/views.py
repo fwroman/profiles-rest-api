938d889a998e4b321c7b08c019e0551cc1dcb974
FREDI ROMAN <kunfelt_f@hotmail.com>
2017-08-22 17:40:01 -0500

    ADDED FILTERS TO SEARCH FUNCTION

--- src/profiles_project/profiles_app/views.py
+++ src/profiles_project/profiles_app/views.py
@@ -9,7 +9,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from rest_framework import status
+# THIS IS USED TO ALLOW ONLY AUTHENTICATED USERS UPDATE OR DELETE INFORMATION:
 from rest_framework.authentication import TokenAuthentication
+
+# THIS IS USED TO FIND USERS BY FILTERS:
+from rest_framework import filters
 
 # IMPORTING SERIALIZERS CLASS
 from . import serializers
@@ -152,3 +156,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
